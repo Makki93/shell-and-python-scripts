5f79cb3cbe72cf3af59d005f27d36cbe68b242e2
Markus Dietl <markus.dietl@mailbox.org>
2024-03-22 09:10:50 +0100

    added function to only group commits with the same jira number

--- squash_repos_past_commits.py
+++ squash_repos_past_commits.py
@@ -46,9 +46,18 @@ def get_branches():
 
 
 def get_canonical_author(author):
-   """Get the canonical author identity by checking against all known identifiers."""
-   # Look up the primary email using the provided author identifier
-   return author_map.get(author.lower(), author)
+    """Get the canonical author identity by checking against all known identifiers."""
+    # Look up the primary email using the provided author identifier
+    return author_map.get(author.lower(), author)
+
+
+def extract_jira_number(commit_message):
+    """
+   Extracts a JIRA issue key (with a project key and a maximum five-digit number) from the commit message.
+   Returns the JIRA key if found, otherwise None.
+   """
+    match = re.search(r'\b[A-Z]+-\d{1,5}\b', commit_message)
+    return match.group() if match else None
 
 
 def squash_commits(branch):
@@ -66,12 +75,12 @@ def squash_commits(branch):
             logging.error(f"Failed to reset branch {branch}: {reset_e}")
         raise  # Re-raise the original exception to stop the script
 
-
     # Get the list of commits in reverse order (oldest first)
     commits = run_git_command(['log', '--reverse', '--pretty=format:%H %an <%ae> %ct', branch]).split('\n')
 
     squashed_commits = []
     current_squash_group = []
+    current_jira_number = None  # Keep track of the current JIRA number
 
     for i, commit_info in enumerate(commits):
         commit_data = commit_info.rsplit(' ', 3)
@@ -100,29 +109,53 @@ def squash_commits(branch):
                 squash_commit_group(current_squash_group, start, end)
                 squashed_commits.append((current_squash_group, end))
             current_squash_group = []  # Reset the squash group
+            current_jira_number = None  # Reset the JIRA number
             continue  # Skip the special commit and continue with the next commit
+
+        # Extract JIRA number from the current commit message
+        commit_jira_number = extract_jira_number(commit_message)
 
         # Determine if the next commit should be added to the current squash group
         if i < len(commits) - 1:
-           next_commit_info = commits[i + 1].rsplit(' ', 3)
-           next_commit, next_author, next_timestamp = next_commit_info[:3]
-           next_author = get_canonical_author(next_author)
-           time_difference = int(next_timestamp) - int(current_timestamp)
+            next_commit_info = commits[i + 1].rsplit(' ', 3)
+            next_commit, next_author, next_timestamp = next_commit_info[:3]
+            next_author = get_canonical_author(next_author)
+            time_difference = int(next_timestamp) - int(current_timestamp)
 
-           # Check if current and next commits are from the same author
-           if current_author == next_author and 0 < time_difference <= SQUASH_TIME_DIFFERENCE:
-               current_squash_group.append((current_commit, current_author))
-           else:
-               # Process the current squash group if the next author is different or the time difference is too large
-               if len(current_squash_group) > 1:
-                   start = current_squash_group[0][0]
-                   end = current_commit
-                   squash_commit_group(current_squash_group, start, end)
-                   squashed_commits.append((current_squash_group, end))
-               current_squash_group = [(current_commit, current_author)]
+            # Check if current and next commits are from the same author
+            if current_author == next_author and 0 < time_difference <= SQUASH_TIME_DIFFERENCE:
+                # Check if the JIRA numbers are the same or if either commit does not have a JIRA number
+                if commit_jira_number == current_jira_number or not commit_jira_number or not current_jira_number:
+                    current_squash_group.append((current_commit, current_author))
+                    current_jira_number = commit_jira_number or current_jira_number
+                else:
+                    # JIRA numbers are different, process the current group and start a new one
+                    if len(current_squash_group) > 1:
+                        start = current_squash_group[0][0]
+                        end = current_squash_group[-1][0]
+                        squash_commit_group(current_squash_group, start, end)
+                        squashed_commits.append((current_squash_group, end))
+                    current_squash_group = [(current_commit, current_author)]
+                    current_jira_number = commit_jira_number
+            else:
+                # Process the current squash group if the next author is different or the time difference is too large
+                if len(current_squash_group) > 1:
+                    start = current_squash_group[0][0]
+                    end = current_commit
+                    squash_commit_group(current_squash_group, start, end)
+                    squashed_commits.append((current_squash_group, end))
+                current_squash_group = [(current_commit, current_author)]
+                current_jira_number = None  # Reset the JIRA number
         else:
-           # If it's the last commit in the list, add it to the current squash group
-           current_squash_group.append((current_commit, current_author))
+            # If it's the last commit in the list, add it to the current squash group
+            current_squash_group.append((current_commit, current_author))
+
+    # Process the last squash group if it has more than one commit
+    if len(current_squash_group) > 1:
+        start = current_squash_group[0][0]
+        end = current_squash_group[-1][0]
+        squash_commit_group(current_squash_group, start, end)
+        squashed_commits.append((current_squash_group, end))
 
     # Log the squashed commits
     for old_commits, new_commit in squashed_commits:
